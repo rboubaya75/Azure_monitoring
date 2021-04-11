5cebb8acfb821bac79210294be27cd7bfc97db93
Bart <bartlomiej.janik@hotmail.fr>
2021-04-11 21:17:09 +0200

    Implemented database queries

--- src/server/app.js
+++ src/server/app.js
@@ -3,21 +3,45 @@ const pg = require('pg')
 
 const app = express()
 
+const pool = new pg.Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+})
 
 function getGeneralCostBySubs(req, res) {
-    res.end("ROUTE1")
+    pool.query("SELECT Name, SUM(Cost) as Total FROM Cost, Subscriptions WHERE Subid=id GROUP BY Name", (err, result) => {
+        if (err) console.log(err)
+        res.json(result.rows)
+    })
 }
 
 function getGeneralCostByServices(req, res) {
-    res.end("ROUTE2")
+    pool.query("SELECT Name, SUM(Cost) as Total FROM Cost, Services WHERE Serviceid=id GROUP BY Name", (err, result) => {
+        if (err) console.log(err)
+        res.json(result.rows)
+    })
 }
 
 function getSubCostByService(req, res) {
-    res.end("ROUTE3")
+    pool.query("SELECT Services.Name as Service, Cost FROM Services, Cost, Subscriptions\
+                WHERE Subid=Subscriptions.id\
+                AND Subscriptions.Name=$1\
+                AND Services.id=Serviceid", [req.params.subscription], (err, result) => {
+        if (err) console.log(err)
+        res.json(result.rows)
+    })
 }
 
 function getServicesCostBySub(req, res) {
-    res.end("ROUTE4")
+    pool.query("SELECT Subscriptions.Name as Subscription, Cost FROM Subscriptions, Cost, Services\
+                WHERE Subid=Subscriptions.id\
+                AND Services.Name=$1\
+                AND Services.id=Serviceid", [req.params.service], (err, result) => {
+        if (err) console.log(err)
+        res.json(result.rows)
+    })
 }
 
 app.get("/api/cost/general/subscriptions", getGeneralCostBySubs)

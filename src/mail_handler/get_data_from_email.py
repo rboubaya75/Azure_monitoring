8e3427513d7fdba0df3cd973a514053e7d70d356
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-04-14 09:12:58 +0200

    Adding a unit test on the get_data_from_email function

--- src/mail_handler/get_data_from_email.py
+++ src/mail_handler/get_data_from_email.py
@@ -5,15 +5,21 @@ import datetime
 from dotenv import load_dotenv 
 load_dotenv()
 
+current_date = datetime.datetime.today().strftime ('%d-%b-%Y')
+imap_url = "imap.gmail.com"
+
+
 def connection_to_inbox(imap_url):
     email_user = os.environ["EMAIL"]
     email_password = os.environ["MOTDEPASSE"]
+
 
     connection = imaplib.IMAP4_SSL(imap_url)
     connection.login(email_user, email_password)
 
     connection.select("inbox") 
     _ , data = connection.fetch(b'12', '(RFC822)') 
+
 
     raw = email.message_from_bytes(data[0][1])
     return raw
@@ -27,6 +33,7 @@ def get_attachement(msg, current_date):
         if part.get('Content-Disposition') is None: 
             continue 
         filename = part.get_filename()
+        print(filename)
 
         if bool(filename): 
             filePath = os.path.join(attachment_folder, filename)
@@ -36,3 +43,5 @@ def get_attachement(msg, current_date):
         old_file = os.path.join(attachment_folder, filename)
         new_file = os.path.join(attachment_folder, f"AZ_Cost_Report_{current_date}.xls")
         os.rename(old_file, new_file)
+
+
